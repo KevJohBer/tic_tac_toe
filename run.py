5a5bb99f0d6df6531f7ec5aa94b02e448f7ac4d3
KevJohBer <kevin.bergstrom0313@gmail.com>
2022-11-15 16:50:36 +0000

    continue work on check winner bug

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import random
+
+
 board = ['.', '.', '.', '.', '.', '.', '.', '.', '.']
+WINNERS = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
+PLAYING = True
+WINNER = False
 
 
 def role_select():
@@ -32,34 +37,38 @@ def choose_place(role):
     """
     Lets user decide where to place their mark.
     """
-    playing = True
-    while playing:
-        for a, b, c in [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]:
-            if role.capitalize() == board[a] == board[b] == board[c]:
-                print(f'\n{role.capitalize()} wins!')
-                playing = False
-                return playing
-
+    global PLAYING
+    global WINNER
+    while PLAYING:
         move = input('\nWhere will you place your mark? ')
         try:
             move = int(move) - 1
             if board[move] == '.':
                 board[move] = role.capitalize()
-                opponent_choose_place(role, playing)
+                opponent_choose_place(role)
+                for a, b, c in WINNERS:
+                    if role.capitalize() == board[a] == board[b] == board[c]:
+                        print(f'\n{role.capitalize()} wins!')
+                        PLAYING = False
+                        WINNER = True
             elif (move > 8) or (move < 0):
                 print('Value must be between 1 and 9\n')
+            elif '.' not in board and not WINNER:
+                PLAYING = False
+                create_board()
+                print("\nIt's a tie!")
             elif board[move] != '.':
                 print(f'\n{move + 1} is already occupied')
         except ValueError:
             print(f'{move} is an invalid input')
-    if '.' not in board:
-        print("It's a tie!")
 
 
-def opponent_choose_place(role, playing):
+def opponent_choose_place(role):
     """
     Makes computer do a move against the player.
     """
+    global WINNER
+    global PLAYING
     computer_move = random.randint(0, 8)
     if role == 'x':
         computer_role = 'o'
@@ -69,15 +78,17 @@ def opponent_choose_place(role, playing):
     if board[computer_move] == '.':
         board[computer_move] = computer_role.capitalize()
         create_board()
-        for a, b, c in [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]:
+        for a, b, c in WINNERS:
             if computer_role.capitalize() == board[a] == board[b] == board[c]:
                 print(f'\n{computer_role.capitalize()} wins!')
-                playing = False
-                return playing
-    elif '.' not in board:
+                PLAYING = False
+                WINNER = True
+    elif '.' not in board and not WINNER:
+        PLAYING = False
         create_board()
+        print("\nIt's a tie!")
     else:
-        opponent_choose_place(role, playing)
+        opponent_choose_place(role)
 
 
 def main():
