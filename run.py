7d8bdd581149f34f8f9eb46d33bba6a25a67f0d7
KevJohBer <kevin.bergstrom0313@gmail.com>
2022-11-16 16:38:16 +0000

    debug check winner and add try again fucntion

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import random
 
 
-board = ['.', '.', '.', '.', '.', '.', '.', '.', '.']
+BOARD = ['.', '.', '.', '.', '.', '.', '.', '.', '.']
 WINNERS = [(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (1, 4, 7), (2, 5, 8), (0, 4, 8), (2, 4, 6)]
 PLAYING = True
 WINNER = False
@@ -29,71 +29,88 @@ def create_board():
     Creates a 3 x 3 playing field which the game will take place on. Also
     checks for wins or ties.
     """
-    print(board[0:3])
+    global WINNER, PLAYING
+    print(BOARD[0:3])
     print()
-    print(board[3:6])
+    print(BOARD[3:6])
     print()
-    print(board[6:9])
+    print(BOARD[6:9])
 
-    global PLAYING, PLAYER1, PLAYER2, WINNERS
     for a, b, c in WINNERS:
-        if (PLAYER1) == board[a] == board[b] == board[c]:
+        if (PLAYER1) == BOARD[a] == BOARD[b] == BOARD[c]:
             print(f'\n{PLAYER1} wins!')
+            WINNER = True
             PLAYING = False
-        elif (PLAYER2) == board[a] == board[b] == board[c]:
+        elif (PLAYER2) == BOARD[a] == BOARD[b] == BOARD[c]:
             print(f'\n{PLAYER2} wins!')
+            WINNER = True
             PLAYING = False
+    if ('.' not in BOARD) and (not WINNER):
+        print("\nIt's a tie!")
+        PLAYING = False
 
 
 def choose_place():
     """
     Lets user decide where to place their mark.
     """
-    global PLAYING, WINNER, PLAYER1
+    global BOARD, PLAYER1, PLAYING
     while PLAYING:
         move = input('\nWhere will you place your mark? ')
         try:
             move = int(move) - 1
-            if board[move] == '.':
-                board[move] = PLAYER1.capitalize()
+            if BOARD[move] == '.':
+                BOARD[move] = PLAYER1
                 opponent_choose_place()
             elif (move > 8) or (move < 0):
                 print('Value must be between 1 and 9\n')
-            elif board[move] != '.':
+            elif BOARD[move] != '.':
                 print(f'\n{move + 1} is already occupied')
         except ValueError:
             print(f'{move} is an invalid input')
+    play_again()
 
 
 def opponent_choose_place():
     """
     Makes computer do a move against the player.
     """
-    global WINNER, PLAYER1, PLAYER2, PLAYING
+    global PLAYER1, PLAYER2, PLAYING
     computer_move = random.randint(0, 8)
     if PLAYER1 == 'X':
         PLAYER2 = 'O'
     elif PLAYER1 == 'O':
         PLAYER2 = 'X'
 
-    if '.' in board:
-        if board[computer_move] == '.':
-            board[computer_move] = PLAYER2
+    if '.' in BOARD:
+        if BOARD[computer_move] == '.':
+            BOARD[computer_move] = PLAYER2
             create_board()
         else:
             opponent_choose_place()
     else:
         create_board()
-        print("\nit's a tie!")
-        PLAYING = False
 
 
-def main():
+def play_again():
     """
-    main
+    Allows the player to choose to play again or not
     """
-    role_select()
+    global BOARD, PLAYING
+    try_again = input('Would you like to play again? ')
+    try:
+        if try_again == 'yes':
+            BOARD = ['.', '.', '.', '.', '.', '.', '.', '.', '.']
+            PLAYING = True
+            create_board()
+            choose_place()
+        elif try_again == 'no':
+            exit()
+    except ValueError():
+        if (try_again != 'yes') and (try_again != 'no'):
+            print(f'{try_again} is not a valid input')
 
 
 print('Welcome to Tic Tac Toe!\n')
-main()
+
+role_select()
