05e2f19392a94b0d54c44dbc63c877a604847a44
KevJohBer <kevin.bergstrom0313@gmail.com>
2022-11-17 10:31:07 +0000

    Clean up unnecesary code and play with graphics

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+"""
+import for usage of random functions
+"""
 import random
 
 
@@ -11,14 +14,23 @@ PLAYER2 = ''
 
 def role_select():
     """
-    Let the user choose between playing as X or O.
+    Let the user choose between playing as X or O. X will always go first.
     """
-    global PLAYER1
-    PLAYER1 = input('Would you like to play as x or o? ').capitalize()
-    if PLAYER1 == 'X' or PLAYER1 == 'O':
-        print(f'You chose {PLAYER1}\n')
-        create_board()
-        choose_place()
+    global PLAYER1, PLAYER2, PLAYING
+    PLAYER1 = input('Would you like to play as x or o? >').capitalize()
+    if PLAYER1 == 'X' or 'O':
+        print(f"""
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+       You chose {PLAYER1}\n
+ooooooooooooooooooooooooooooooooooo
+        """)
+        if PLAYER1 == 'X':
+            PLAYER2 = 'O'
+            choose_place()
+        elif PLAYER1 == 'O':
+            PLAYER2 = 'X'
+            opponent_choose_place()
+            choose_place()
     else:
         print(f'{PLAYER1} is not a valid input.\n')
         role_select()
@@ -30,24 +42,11 @@ def create_board():
     checks for wins or ties.
     """
     global WINNER, PLAYING
-    print(BOARD[0:3])
+    print(f'         {BOARD[0:3]}')
     print()
-    print(BOARD[3:6])
+    print(f'         {BOARD[3:6]}')
     print()
-    print(BOARD[6:9])
-
-    for a, b, c in WINNERS:
-        if (PLAYER1) == BOARD[a] == BOARD[b] == BOARD[c]:
-            print(f'\n{PLAYER1} wins!')
-            WINNER = True
-            PLAYING = False
-        elif (PLAYER2) == BOARD[a] == BOARD[b] == BOARD[c]:
-            print(f'\n{PLAYER2} wins!')
-            WINNER = True
-            PLAYING = False
-    if ('.' not in BOARD) and (not WINNER):
-        print("\nIt's a tie!")
-        PLAYING = False
+    print(f'         {BOARD[6:9]}')
 
 
 def choose_place():
@@ -56,11 +55,14 @@ def choose_place():
     """
     global BOARD, PLAYER1, PLAYING
     while PLAYING:
+        create_board()
         move = input('\nWhere will you place your mark? ')
         try:
             move = int(move) - 1
             if BOARD[move] == '.':
                 BOARD[move] = PLAYER1
+                if not WINNER:
+                    check_winner(PLAYER1)
                 opponent_choose_place()
             elif (move > 8) or (move < 0):
                 print('Value must be between 1 and 9\n')
@@ -68,6 +70,7 @@ def choose_place():
                 print(f'\n{move + 1} is already occupied')
         except ValueError:
             print(f'{move} is an invalid input')
+    create_board()
     play_again()
 
 
@@ -75,42 +78,65 @@ def opponent_choose_place():
     """
     Makes computer do a move against the player.
     """
-    global PLAYER1, PLAYER2, PLAYING
+    global PLAYER2, BOARD
     computer_move = random.randint(0, 8)
-    if PLAYER1 == 'X':
-        PLAYER2 = 'O'
-    elif PLAYER1 == 'O':
-        PLAYER2 = 'X'
 
     if '.' in BOARD:
         if BOARD[computer_move] == '.':
             BOARD[computer_move] = PLAYER2
-            create_board()
+            if not WINNER:
+                check_winner(PLAYER2)
         else:
             opponent_choose_place()
-    else:
-        create_board()
+
+
+def check_winner(player):
+    """
+    checks if there is a winner or tie
+    """
+    global PLAYING, WINNER
+    for a, b, c in WINNERS:
+        if (player) == BOARD[a] == BOARD[b] == BOARD[c]:
+            print(f"""
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+             {player} wins!
+ooooooooooooooooooooooooooooooooooo
+            """)
+            WINNER = True
+            PLAYING = False
+
+    if ('.' not in BOARD) and (not WINNER):
+        print("""
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+           It's a tie!
+ooooooooooooooooooooooooooooooooooo
+        """)
+        PLAYING = False
 
 
 def play_again():
     """
     Allows the player to choose to play again or not
     """
-    global BOARD, PLAYING
-    try_again = input('Would you like to play again? ')
-    try:
+    global BOARD, PLAYING, WINNER
+    try_again = input('\nWould you like to play again? ')
+    while not PLAYING:
         if try_again == 'yes':
             BOARD = ['.', '.', '.', '.', '.', '.', '.', '.', '.']
             PLAYING = True
-            create_board()
+            WINNER = False
             choose_place()
         elif try_again == 'no':
             exit()
-    except ValueError():
-        if (try_again != 'yes') and (try_again != 'no'):
+        else:
             print(f'{try_again} is not a valid input')
+            play_again()
 
 
-print('Welcome to Tic Tac Toe!\n')
+print("""
+xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+      Welcome to Tic Tac Toe!
+ooooooooooooooooooooooooooooooooooo
+""")
 
 role_select()
